a55fa9958a6cf380b416842a751dd0bc195bbdeb
TOSHIBA <wolesijohn@gmail.com>
2024-05-26 14:59:13 +0100

    additon of delete & update features on the app

--- food/urls.py
+++ food/urls.py
@@ -5,4 +5,8 @@ urlpatterns=[
     path('',views.index,name='index'),
     path('<int:item_id>/',views.details,name='details'),
     path('items/',views.items,name='items'),
+    path('add',views.create_item,name="create_item"),
+    path('update/<int:id>/',views.update_item,name="update_item"),
+    path('delete/<int:id>/',views.delete_item,name="delete_item"),
+    
 ]
