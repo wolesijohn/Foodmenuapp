972e29fcc5c2ccddae595b761ad930c3f3ff61ad
TOSHIBA <wolesijohn@gmail.com>
2024-06-04 20:58:04 +0100

    insert form fields user

--- users/views.py
+++ users/views.py
@@ -4,16 +4,21 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.shortcuts import redirect
+from .forms import NewUserForm
 
 # Create your views here.
+
+
+    
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = NewUserForm()
     return render(request, 'users/register.html', {'form': form})
