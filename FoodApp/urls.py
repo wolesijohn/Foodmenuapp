1a0827e02b12f533303da5896d9dd97b26208dbd
wolesijohn <104014371+wolesijohn@users.noreply.github.com>
2024-06-09 02:21:58 +0000

    addition of login/logout functionality

--- FoodApp/urls.py
+++ FoodApp/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from users import views as users_views
+from django.contrib.auth import views as authentication_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/',include('food.urls')),
+    path('register/',users_views.register,name='register'),
+    path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+
     
 ]
